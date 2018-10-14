9fc77290e283dd9a517d3a7d2c5c4b0fc0ca0519
hlilou5 <40966905+hlilou5@users.noreply.github.com>
2018-10-14 23:57:07 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+var x = prompt("username");
+var y = prompt("password");
+
+	if (x === "nour",y === "hlilou") {
+
+}
+	if (x === "hlilou",y === "hlilou") {
+
+}
+	if (x === "sarah",y === "hlilou") {
+
+}
+	if (x === "zeineb",y === "hlilou") {
+window.alert("welcome"+" "+x);
+}
+
+
+
+
+
+
+
+
+else  {
+	window.alert("rou7");
+	document.getElementById('main-div').style.display='none'
+}
